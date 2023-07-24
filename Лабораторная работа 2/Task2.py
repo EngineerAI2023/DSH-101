88b9b8c89a8360474d38ae5c40e8f2efb87d4e49
EngineerAI2023 <123943452+EngineerAI2023@users.noreply.github.com>
2023-07-24 16:35:33 +0300

    Add files via upload

--- Лабораторная работа 2/Task2.py
+++ Лабораторная работа 2/Task2.py
@@ -5,7 +5,7 @@ increase = 0.03  # Ежемесячный рост цен
 
 # TODO Рассчитайте подушку безопасности, чтобы протянуть 10 месяцев без долгов
 total = spend
-for i in range(2,months + 1):
+for _ in range(2,months + 1):
     spend = spend * (increase + 1)
     total = total + spend
 
